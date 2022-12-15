75183271d1e01af2be45f08eb219c19771f94a4f
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2022-12-16 00:38:43 +0800

    Connect to homepage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,75 +1,52 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:twitter_clonee/Screens/Widgets/drawerslide.dart';
+import 'package:twitter_clonee/Screens/spaces.dart';
 import 'package:twitter_clonee/accounts/useracc.dart';
 import './post/tweets.dart';
 import './botnav/botnavig.dart';
 import 'Screens/homescreen.dart';
 import 'Screens/searchscreen.dart';
 
+final scaffoldKey = GlobalKey<ScaffoldState>();
 
- final scaffoldKey = GlobalKey<ScaffoldState>();
-
- Useracc user = Useracc();
+Useracc user = Useracc();
 
 Tweets tweets = Tweets();
 
-
-
-
-class HomePage extends StatefulWidget{
- static const routename = './homepage/';
+class HomePage extends StatefulWidget {
+  static const routename = './homepage/';
 
   const HomePage({super.key});
 
-  
   @override
   Homepage createState() => Homepage();
-
-
 }
-
-
-
 
 class Homepage extends State<HomePage> with TickerProviderStateMixin {
   String src = "https://cdn-icons-png.flaticon.com/512/733/733579.png";
 
- 
- late TabController tabController;
-  
+  late TabController tabController;
 
-
- @override
+  @override
   void initState() {
     super.initState();
     tabController = TabController(initialIndex: 0, length: 5, vsync: this);
   }
 
-   
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      key: scaffoldKey,
-          drawer: DrawerS(user),
-      
-     body: TabBarView (controller: tabController,children: [
-         HomeScreen(tweets,user),
-          SearchScreen(),
-     ],),
-
-     bottomNavigationBar: botnav(context, tabController)
-        );
-        
-    
-  
-    
-    
+        backgroundColor: Colors.white,
+        key: scaffoldKey,
+        drawer: DrawerS(user),
+        body: TabBarView(
+          controller: tabController,
+          children: [
+            HomeScreen(tweets, user),
+            const SearchScreen(),
+            const Spacess(),
+          ],
+        ),
+        bottomNavigationBar: botnav(context, tabController));
   }
 }
